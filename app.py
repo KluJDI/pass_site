65aceec8141f40a5d544706d7277f3e187679191
KluJDI <124577325+KluJDI@users.noreply.github.com>
2025-06-25 22:10:04 +0500

    Update app.py

--- app.py
+++ app.py
@@ -110,5 +110,9 @@ def generate():
 
     return send_file(output, download_name="Паспорт_безопасности.docx", as_attachment=True)
 
+import os
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
+
